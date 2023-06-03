e7c5bfae62be2a251b7eeca1f58b1fecb12e8637
Farhadsb98 <124209727+Farhadsb98@users.noreply.github.com>
2023-06-03 11:29:48 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// var UserBalance = 1000;
+// var UserPassword = "8585";
+
+// var tryCount = 3;
+// while (tryCount > 0) {
+//   var password = prompt("parolu daxil edin");
+//   if (password === UserPassword) {
+//     console.log(`balansinizdaki mebleg ${UserBalance}`);
+//     var amount = Number(prompt("meblegi daxil edin"));
+//   }
+// }
